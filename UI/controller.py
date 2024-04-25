54fbef60023356f8f52bd6f88c913852ccc74b6b
CagninMattia <s297196@studenti.polito.it>
2024-04-25 16:57:36 +0200

    Finito

--- UI/controller.py
+++ UI/controller.py
@@ -1,3 +1,5 @@
+import copy
+
 import flet as ft
 
 from UI.view import View
@@ -12,15 +14,96 @@ class Controller:
         self._model = model
         # other attributes
         self._mese = 0
+        self.costo_min = float("inf")
+        self.sequenza_migliore = []
 
     def handle_umidita_media(self, e):
-        pass
-
-
+        mese = self._view.dd_mese.value
+        (um_ge, um_mi, um_to) = self._model.trova_umidita_media(mese)
+        self._view.lst_result.controls.append(ft.Text(f"L'umidità media nel mese selezionato è: \n Genova: {um_ge} \n Milano: {um_mi} \n Torino: {um_to}"))
+        self._view.update_page()
 
     def handle_sequenza(self, e):
-        pass
+        mese = self._view.dd_mese.value
+        diz_citta = self._model.trova_umidita_primi_15(mese)
+
+        def calculate_cost(lista):
+            c = 0
+            for i in range(len(lista) - 1):
+                if i != 0:
+                    if lista[i].localita != lista[i + 1].localita:
+                        c += 1
+            cc = 0
+            for i in lista:
+                cc += i.umidita
+            costo_temp = cc + c * 100 + 200
+            return costo_temp
+
+        def controllo_citta(lista):
+            citta = []
+            for i in lista:
+                citta.append(i.localita)
+            for i in lista:
+                if citta.count(i.localita) > 6:
+                    return False
+            return True
+
+        def controllo_costo(lista, costo_min):
+            c = 0
+            for i in range(len(lista) - 1):
+                if i != 0:
+                    if lista[i].localita != lista[i + 1].localita:
+                        c += 1
+            cc = 0
+            for i in lista:
+                cc += i.umidita
+            costo_temp = cc + c * 100 + 200
+            if costo_temp > costo_min:
+                return False
+            else:
+                return True
+
+        def controllo_staz_min(lista, citta):
+            if len(lista) == 1:
+                if lista[-1].localita == citta:
+                    return True
+                else:
+                    return False
+            else:
+                if lista[-1].localita == lista[-2].localita == citta:
+                    return True
+                else:
+                    if len(lista) >= 4 and lista[-2].localita == lista[-3].localita == lista[-4].localita:
+                        return True
+                    else:
+                        return False
+
+        def recursive_optimize(days, current_sequence, diz_giorni_citta):
+            # Provo tutte le possibili combinazioni e vedo quella di costo minore
+            # Pago solo per i primi due giorni di stazionamento
+
+            if days > 15:
+                if calculate_cost(current_sequence) < self.costo_min:
+                    self.costo_min = calculate_cost(current_sequence)
+                    self.sequenza_migliore = copy.deepcopy(current_sequence)
+                return
+
+            for c in diz_giorni_citta:
+                for giorno in range(len(diz_giorni_citta[c])):
+                    if int(diz_giorni_citta[c][giorno].data.day) == int(days):
+                        current_sequence.append(copy.deepcopy(diz_giorni_citta[c][giorno]))
+                        if (controllo_citta(current_sequence) and controllo_costo(current_sequence, self.costo_min)
+                                and controllo_staz_min(current_sequence, c)):
+                            recursive_optimize(days+1, current_sequence, diz_giorni_citta)
+                        current_sequence.pop()
+
+        recursive_optimize(1, [], diz_citta)
+        self._view.lst_result.controls.append(ft.Text(f"La sequenza ha costo ottimo {self.costo_min} ed è:"))
+        for seq in self.sequenza_migliore:
+            self._view.lst_result.controls.append(ft.Text(f"    [{seq.localita} - {seq.data}] Umidità = {seq.umidita}"))
+        self._view.update_page()
 
     def read_mese(self, e):
         self._mese = int(e.control.value)
+
 
--- model/model.py
+++ model/model.py
@@ -1,3 +1,56 @@
+from database.meteo_dao import MeteoDao
+
+
 class Model:
     def __init__(self):
-        pass
+        self.lista_meteo = []
+
+    def trova_umidita_media(self, mese):
+        if len(self.lista_meteo) == 0:
+            self.lista_meteo = MeteoDao.get_all_situazioni()
+        lista_meteo_mensile = []
+        for sit in self.lista_meteo:
+            if sit.data.month == int(mese):
+                lista_meteo_mensile.append(sit)
+        lista_meteo_mensile_genova = []
+        lista_meteo_mensile_milano = []
+        lista_meteo_mensile_torino = []
+        um_ge = 0
+        um_to = 0
+        um_mi = 0
+        for sit in lista_meteo_mensile:
+            if sit.localita == "Genova":
+                lista_meteo_mensile_genova.append(sit)
+            if sit.localita == "Milano":
+                lista_meteo_mensile_milano.append(sit)
+            if sit.localita == "Torino":
+                lista_meteo_mensile_torino.append(sit)
+        for sit in lista_meteo_mensile_genova:
+            umidita = sit.umidita
+            um_ge += umidita
+        um_ge = um_ge / len(lista_meteo_mensile_genova)
+        for sit in lista_meteo_mensile_milano:
+            umidita = sit.umidita
+            um_mi += umidita
+        um_mi = um_mi / len(lista_meteo_mensile_milano)
+        for sit in lista_meteo_mensile_torino:
+            umidita = sit.umidita
+            um_to += umidita
+        um_to = um_to / len(lista_meteo_mensile_torino)
+        return um_ge, um_mi, um_to
+
+    def trova_umidita_primi_15(self, mese):
+        if len(self.lista_meteo) == 0:
+            self.lista_meteo = MeteoDao.get_all_situazioni()
+        diz_meteo_mensile_by_city = {}
+        for sit in self.lista_meteo:
+            if sit.data.month == int(mese) and sit.data.day <= 15:
+                city = sit.localita
+                if city not in diz_meteo_mensile_by_city:
+                    diz_meteo_mensile_by_city[city] = []
+                diz_meteo_mensile_by_city[city].append(sit)
+
+        for city in diz_meteo_mensile_by_city:  # Itera direttamente sulle chiavi del dizionario
+            diz_meteo_mensile_by_city[city] = sorted(diz_meteo_mensile_by_city[city], key=lambda x: x.data.day)
+
+        return diz_meteo_mensile_by_city
